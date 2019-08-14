bbec824401feec56322bfaa6f44e94d49207cda0
Danny <danny.yi.weng@gmail.com>
2019-08-13 20:35:04 -0400

    Major changes to include GUI now

--- README.md
+++ README.md
@@ -1,32 +1,40 @@
-# Link Analyzer - Python 3  
+# Link Analyzer
 
-## You need to complete the following steps to have the script run properly.  
-It is important to not expose your API Keys!!!  
-To handle this concern, I have hidden the 'config.py' from future git commits.
-### Create a file named 'config.py' on the root directory of this repo and add the following (replace 'API-KEY' with your keys):  
+## Contents
+ - [Current Features](#linkAnalyzer-can-currently)
+ - [Requirements](#requirements)
+ - [How to Use](#how-to-use-script)
+
+## linkAnalyzer can Currently:
+
+  - Unshorten URL's that have been shortened by external services.
+  - Perform reputation checks from:
+    - [VirusTotal](https://www.virustotal.com)
+    - [URLscan](https://www.urlscan.io/)
+  - Get links and compare them against [VirusTotal](https://www.virustotal.com) (see requirements)
+  - Get links and compare them against [URLscan](https://www.urlscan.io/) to see screenshot of page.   
+
+## Requirements
+ - [Python 3.x](https://www.python.org/)
+ - Install all dependencies from the requirements.txt file. `pip install -r requirements.txt`
+### You need to complete the following steps to have the script run properly.  
+It is important to not expose your API Keys!  
+To handle this concern, I've placed the API keys in a file named 'config.py'.
+#### Create a file named 'config.py' on the root directory of this repo and add the following (replace 'API-KEY' with your keys):  
 
 ```
 urlScanAPIKey = 'API-KEY'
 vtAPIKey = 'API-KEY' 
 ```
 
-Make sure you have python 3 and dependencies installed.  
-Run the command below to install the dependencies:  
-
-```
-pip install -r requirements.txt
-```
-
+## How to use script
 The main script file is linkAnalyzer.py  
 To run the script,  
 Open CMD, PowerShell or terminal and enter :  
 
 ```
 python linkAnalyzer.py
-```
+```   
 
-#### Tools to be intergrated:  
-- unshorten.me
-- UrlScan.io
-- VirusTotal
-- More coming...
+#### Idea Board
+- whois
---
+++ linkAnalyzer_GUI.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+
+# Link Analyzer Script/Toolkit
+# Author: Danny Weng
+# Reference:
+# https://github.com/dannyweng
+
+try:
+	## import argparse for future implementations
+    import sys
+    import PySimpleGUI as sg
+    import subprocess    
+    from modules import urlScan
+    from modules import unShortenLink
+    from modules import virusTotal
+    
+
+# Any import errors print to screen and exit
+except (Exception, error):
+	print ('error')
+	sys.exit(1)
+
+# Please check Demo programs for better examples of launchers      
+def ExecuteCommandSubprocess(command, *args):      
+    try:      
+        sp = subprocess.Popen([command, *args], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)      
+        out, err = sp.communicate()      
+        if out:      
+            print(out.decode("utf-8"))      
+        if err:      
+            print(err.decode("utf-8"))      
+    except:      
+        pass      
+
+
+layout = [      
+    [sg.Text('1. Input URL    2. Click on desired script or hit ENTER to run all', size=(50, 1))],      
+    [sg.Output(size=(88, 20))],      
+    [sg.Button('unShorten Link'), sg.Button('urlScan'), sg.Button('VirusTotal'), sg.Button('EXIT')],      
+    [sg.Text('Paste link here:', size=(15, 1)), sg.InputText(focus=True), sg.Button('Run All', bind_return_key=True)]      
+        ]      
+
+
+window = sg.Window('Script launcher - by Danny Weng', layout)      
+
+# ---===--- Loop taking in user input and using it to call scripts --- #      
+
+
+while True:      
+  (event, value) = window.Read()      
+  if event == 'EXIT'  or event is None:  
+      break # exit button clicked  
+  if event == 'unShorten Link':  
+      unShortenLink.shortenLink(value[0]) # unshorten link
+  elif event == 'urlScan':  
+      urlScan.urlScanFunc(value[0])    ## urlScan.io 
+  elif event == 'VirusTotal':  
+      virusTotal.vtFunc(value[0])    ## VirusTotal
+  elif event == 'Run All':      
+    #   ExecuteCommandSubprocess(value[0])
+      user_input = unShortenLink.shortenLink(value[0]) # unshorten link
+      urlScan.urlScanFunc(user_input)    ## urlScan.io
+      virusTotal.vtFunc(user_input)    ## VirusTotal
+      print ('\nThe script opened up a few tabs for you to review.\n\nfyi: it takes urlscan about a whole minute to scan the site so wait a minute and refresh page')
